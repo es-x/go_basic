b6f1a0dd4cb9524f19d4cf52889d8979daa4d826
Олег Владимирович Евдокимов <git@esx.su>
2023-12-29 11:45:55 +0300

    HW03 complete

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -12,20 +12,15 @@ func main() {
 	fmt.Scanf("%d", &s)
 
 	for i := 1; i <= s; i++ {
-		if i&1 != 0 {
-			if s%2 == 0 {
-				fmt.Println(strings.Repeat("# ", s/2))
-			} else {
-				fmt.Println(strings.Repeat("# ", s/2) + "#")
-			}
-
-		} else {
-			if s%2 == 0 {
-				fmt.Println(strings.Repeat(" #", s/2))
-			} else {
-				fmt.Println(strings.Repeat(" #", s/2) + " ")
-			}
+		switch {
+		case i&1 != 0 && s%2 == 0:
+			fmt.Println(strings.Repeat("# ", s/2))
+		case i&1 != 0 && s%2 != 0:
+			fmt.Println(strings.Repeat("# ", s/2) + "#")
+		case i&1 != 1 && s%2 == 0:
+			fmt.Println(strings.Repeat(" #", s/2))
+		case i&1 != 1 && s%2 != 0:
+			fmt.Println(strings.Repeat(" #", s/2) + " ")
 		}
-
 	}
 }
