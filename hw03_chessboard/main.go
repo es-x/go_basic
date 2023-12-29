7edc890d0ce8604a1046cab36f5da06fbb102e43
Олег Владимирович Евдокимов <git@esx.su>
2023-12-29 11:13:06 +0300

    HW03 complete

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	// Place your code here.
+	var s int
+
+	fmt.Printf("Введите размер сетки: ")
+	fmt.Scanf("%d", &s)
+
+	for i := 1; i <= s; i++ {
+		if i&1 != 0 {
+			if s%2 == 0 {
+				fmt.Println(strings.Repeat("# ", s/2))
+			} else {
+				fmt.Println(strings.Repeat("# ", s/2) + "#")
+			}
+
+		} else {
+			if s%2 == 0 {
+				fmt.Println(strings.Repeat(" #", s/2))
+			} else {
+				fmt.Println(strings.Repeat(" #", s/2) + " ")
+			}
+		}
+
+	}
 }
